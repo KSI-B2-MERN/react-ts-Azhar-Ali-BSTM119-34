a509603ca4e300224355b6614612a07615963a80
Azhar Ali <101246889+Azhar-Ali-BSTM119-34@users.noreply.github.com>
2023-10-20 12:16:14 +0500

    fifth commit

--- src/Component/registerCard/registerCard.tsx
+++ src/Component/registerCard/registerCard.tsx
@@ -36,13 +36,19 @@ const RegisterCard: React.FC = () => {
       <div>
         <label htmlFor="password">Password:</label>
         <input
+          className=""
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
       </div>
-      <button type="submit">Register</button>
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        type="submit"
+      >
+        Register
+      </button>
     </form>
   );
 };
