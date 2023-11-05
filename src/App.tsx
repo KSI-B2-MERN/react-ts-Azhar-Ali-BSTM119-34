c2032c4dbdb4fa0c715df8958ca1594152ede5fd
Azhar Ali <101246889+Azhar-Ali-BSTM119-34@users.noreply.github.com>
2023-11-06 02:19:20 +0500

    last commit of 06-11-2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Signup from "./Component/registerCard/signUp";
+import LogIn from "./pages/LogIn";
 
 const App: React.FC = () => {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1>this is header</h1>
+
+      <LogIn />
     </div>
   );
 };
--- src/pages/LogIn.tsx
+++ src/pages/LogIn.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function LogIn() {
-  return <div>LogIn</div>;
+  return (
+    <div className="h-3/4 w-screen bg-slate-200">
+      <div className="h-2/5 w-screen flex flex-row justify-center items-center">
+        <h1>Login </h1>
+        <p>Please enter your Username and Password </p>
+      </div>
+      <div>
+        <div>login picture</div>
+        <div>form</div>
+      </div>
+    </div>
+  );
 }
 
 export default LogIn;
